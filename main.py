4ec7e5db1dcc1e47b076e6284f7b3ee697a8928d
chr1s <39552369+TheRealchr1s@users.noreply.github.com>
2022-05-11 14:31:43 -0400

    toekn change

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ with open("config.json", "r") as f:
 app = quart.Quart(__name__)
 app.register_blueprint(v1_api)
 app.rate_limiter = quart_rate_limiter.RateLimiter(app, store=RedisStore(config["REDIS_URI"]))
+app.token_cache = dict()
 
 async def init_postgres():
     app.db = await asyncpg.create_pool(config["POSTGRES_URI"])
     async with app.db.acquire() as conn:
         await conn.execute("CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(15), id BIGINT);")
+        for entry in (await conn.fetchall("SELECT * FROM tokens;")):
+            app.token_cache[entry.get("id")] = entry.get("token")
 
 async def gen_and_save_token(user):
     token = secrets.token_urlsafe(15)
@@ -31,6 +34,7 @@ async def gen_and_save_token(user):
             async with connection.transaction():
                 await connection.execute("DELETE FROM tokens WHERE id = $1;", user.id)
                 await connection.execute("INSERT INTO tokens VALUES ($1, $2);", token, user.id)
+    app.token_cache[user.id] = token
     return token
 
 if config.get("POSTGRES_URI"):
@@ -67,11 +71,12 @@ async def callback():
 @requires_authorization
 async def token_route():
     user = await discord.get_authorization_token()
-    async with quart.current_app.db.acquire() as connection:
-        token = (await connection.fetchrow("SELECT token FROM tokens WHERE id = $1", user.id)).get("token")
+    token = app.token_cache.get(user.id)
+    # async with quart.current_app.db.acquire() as connection:
+    #     token = (await connection.fetchrow("SELECT token FROM tokens WHERE id = $1", user.id)).get("token")
     if not token:
         token = await app.gen_token()
-    return await quart.render_template("tokenpage.html", token="Example")
+    return await quart.render_template("tokenpage.html", token=token)
 
 @app.route("/demo/<end>")
 @requires_authorization
