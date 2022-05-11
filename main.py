501cb5ec4b3b146551aa5b388bc66b9ca86e810b
chr1s <39552369+TheRealchr1s@users.noreply.github.com>
2022-05-11 17:22:48 -0400

    yo

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ if config.get("POSTGRES_URI"):
     async def init_postgres():
         app.db = await asyncpg.create_pool(config["POSTGRES_URI"])
         async with app.db.acquire() as conn:
-            await conn.execute("CREATE TABLE IF NOT EXISTS tokens (token VARCHAR(15), id BIGINT);")
+            await conn.execute("CREATE TABLE IF NOT EXISTS tokens (token TEXT, id BIGINT);")
             for entry in (await conn.fetch("SELECT * FROM tokens;")):
                 app.token_cache[entry.get("id")] = entry.get("token")
 
