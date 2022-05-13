a3945fa809ab2ce4ba1dd8f3a5778baf5d33f6ee
chr1s <39552369+TheRealchr1s@users.noreply.github.com>
2022-05-13 19:25:34 -0400

    oops

--- main.py
+++ main.py
@@ -51,8 +51,6 @@ async def init_postgres():
     else:
         app.token_cache[246938839720001536] = "TESTING_PURPOSES"
 
-app.logger.warn("hi")
-
 async def commit_usage_data():
     try:
         while True:
@@ -66,16 +64,16 @@ async def commit_usage_data():
                     to_del = []
                     current = await connection.fetch("SELECT * FROM usage;")
                     for usr in app.usage_cache:
-                        for endpi in app.usage_cache[usr].values():
+                        for endpi in app.usage_cache[usr].keys():
+                            curr_ind = 0
                             for row in current:
-                                curr_ind = 0
                                 if (row.get("endpoint") == endpi) and (row.get("id") == usr):
                                     curr_ind = row.get("count")
                                     to_del.append((row.get("endpoint"), row.get("id")))
-                                entries.append((endpi, usr, app.usage_cache[usr][endpi]+curr_ind))
+                            entries.append((endpi, usr, app.usage_cache[usr][endpi]+curr_ind))
                     await connection.executemany("DELETE FROM usage WHERE endpoint=$1 AND id=$2;", to_del)
                     await connection.executemany("INSERT INTO usage VALUES ($1, $2, $3);", entries)
-                app.logger.warn(await connection.fetch("SELECT * FROM usage;"))
+                    app.logger.warn(await connection.fetch("SELECT * FROM usage;"))
             await asyncio.sleep(100)
     except asyncio.CancelledError:
         pass
